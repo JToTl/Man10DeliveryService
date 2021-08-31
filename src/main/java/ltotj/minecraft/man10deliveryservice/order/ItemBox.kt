5c0b7de16e088cee6b20fdc4b46bb1e1df02826d
JToTl <80580108+JToTl@users.noreply.github.com>
2021-08-31 09:47:32 +0900

    Add files via upload

---
+++ src/main/java/ltotj/minecraft/man10deliveryservice/order/ItemBox.kt
@@ -0,0 +1,90 @@
+package ltotj.minecraft.man10deliveryservice.order
+
+import ltotj.minecraft.man10deliveryservice.Main
+import ltotj.minecraft.man10deliveryservice.Main.Companion.boxOpeningList
+import ltotj.minecraft.man10deliveryservice.Main.Companion.executor
+import ltotj.minecraft.man10deliveryservice.Main.Companion.pluginTitle
+import ltotj.minecraft.man10deliveryservice.MySQLManager
+import ltotj.minecraft.man10deliveryservice.Utility.countAirPocket
+import ltotj.minecraft.man10deliveryservice.Utility.getDateForMySQL
+import ltotj.minecraft.man10deliveryservice.Utility.getNBTInt
+import ltotj.minecraft.man10deliveryservice.Utility.itemFromBase64
+import org.bukkit.Sound
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
+import java.util.*
+
+object ItemBox: Listener {
+
+    private val mysql= MySQLManager(Main.plugin, pluginTitle)
+
+    @EventHandler
+    fun containerOpenEvent(e: PlayerInteractEvent){
+        val item=e.item?:return
+        val order_id=getNBTInt(item,"order_id")
+        if(order_id==-1)return
+        e.isCancelled=true
+        if(e.hand==null||(e.hand!=null&&e.hand!=EquipmentSlot.HAND))return
+        if(e.player.hasPermission("mdlivery.admin")&&getNBTInt(item,"admin_box")==1){
+            executor.execute{
+                val result= mysql.query("select amount,slot1,slot2,slot3,slot4,slot5,slot6,slot7,slot8 from delivery_order where order_id=$order_id;")?:return@execute
+                result.next()
+                if(result.row==0){
+                    e.player.sendMessage("§4アイテムボックスのデータが見つかりませんでした")
+                }
+                else if(countAirPocket(e.player.inventory)<result.getInt("amount")){
+                    e.player.sendMessage("§4インベントリに§d${countAirPocket(e.player.inventory)}個§4の空きを作ってください")
+                }
+                else{
+                    for(i in 1..8){
+                        val boxedItem= itemFromBase64(result.getString("slot$i")?:continue)?:continue
+                        e.player.inventory.addItem(boxedItem)
+                    }
+                    e.player.sendMessage("§aアドミンボックスを開封しました")
+                    e.player.inventory.remove(item)
+                }
+                result.close()
+                mysql.close()
+                return@execute
+            }
+        }
+        else if(!Main.available){
+            e.player.sendMessage("§4[${Main.pluginTitle}]はただいま停止中です")
+            return
+        }
+        else{
+            if(boxOpeningList.contains(e.player.uniqueId))return
+            boxOpeningList.add(e.player.uniqueId)
+            executor.execute {
+                val result=mysql.query("select amount,slot1,slot2,slot3,slot4,slot5,slot6,slot7,slot8,box_status from delivery_order where order_id=$order_id;")?:return@execute
+                result.next()
+                if(result.row==0){
+                    e.player.sendMessage("§4アイテムボックスが見つかりませんでした")
+                }
+                else if(result.getBoolean("box_status")){
+                    e.player.sendMessage("§4既に開封されたボックスです")
+                    println("§4${e.player.name}が開封済みのボックスを所持していました：order_id-$order_id")
+                    e.player.inventory.remove(item)
+                }
+                else if(countAirPocket(e.player.inventory)<result.getInt("amount")){
+                    e.player.sendMessage("§4インベントリがいっぱいです §d${countAirPocket(e.player.inventory)}個§4の空きを作って再度開けてください")
+                }
+                else{
+                    mysql.execute("update delivery_order set box_status=true,opener_name='${e.player.name}',opener_uuid='${e.player.uniqueId}',opened_date='${getDateForMySQL(Date())}' where order_id=$order_id;")
+                    for(i in 1..8) {
+                        val boxedItem = itemFromBase64(result.getString("slot$i") ?: continue) ?: continue
+                        e.player.inventory.addItem(boxedItem)
+                    }
+                    e.player.inventory.remove(item)
+                    e.player.playSound(e.player.location, Sound.ENTITY_PLAYER_LEVELUP,1F,1F)
+                    e.player.sendMessage("§aボックスを開封しました")
+                    boxOpeningList.remove(e.player.uniqueId)
+                }
+                boxOpeningList.remove(e.player.uniqueId)
+            }
+        }
+    }
+}
---
+++ src/main/resources/db.sql
@@ -0,0 +1,38 @@
+create table delivery_order
+(
+    order_id int unsigned auto_increment,
+    sender_name varchar(16) null,
+    sender_uuid varchar(36) null,
+    receiver_name varchar(16) null,
+    receiver_uuid varchar(36) null,
+    order_date DATETIME null,
+    receive_date DATETIME null,
+    order_status boolean default false,
+    wrapping varchar(16) default 'cardboard',
+    boxName varchar(50) default '段ボール箱',
+    amount tinyint not null,
+    slot1 longtext null,
+    slot2 longtext null,
+    slot3 longtext null,
+    slot4 longtext null,
+    slot5 longtext null,
+    slot6 longtext null,
+    slot7 longtext null,
+    slot8 longtext null,
+    box_status boolean default false,
+    opener_name varchar(16) null,
+    opener_uuid varchar(36) null,
+    opened_date DATETIME null,
+
+    primary key(order_id)
+);
+
+create table player_status
+(
+    player_id int unsigned auto_increment,
+    owner_name varchar(16) null,
+    owner_uuid varchar(36) not null,
+    delivery_amount int default 0,
+
+    primary key(player_id)
+);
